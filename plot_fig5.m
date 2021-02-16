53e62c632bdc24665347b1d76041ae2a78e9ef38
Carmen <kohl.carmen.1@gmail.com>
2021-02-16 12:12:14 -0500

    standardised formatting

--- plot_fig5.m
+++ plot_fig5.m
@@ -11,37 +11,44 @@
 clear
 dir = fileparts(which('plot_fig5.m'));
 cd(dir)
+
+% Conditions
 Hemi = {'L', 'R',}; %left/right hemisphere
 Tone = {'Contra','Ipsi'}; %contra/ipsilateral tone presentation
 
-%% Figure 5
-figure
+% Figure Settings
 ylims = [-60 20];
 xlims = [0 250];
 colours = {[109 187 228 127], [226 113 113 127]; [41 47 123], [126 20 22]};
+
+% Figure
+figure
 for hemi=1:length(Hemi)
     subplot(1,length(Hemi),hemi)
     hold on
-    title(strcat('Hemi: ',Hemi{hemi}))
+    title(strcat('Hemisphere: ',Hemi{hemi}))
     xlabel('Time (ms)')
     ylabel('Amplitude (nAm)')
     xlim(xlims)
     ylim(ylims)
     for tone = 1:length(Tone)
+        
         % load MEG data
         data = load(strcat('MEG_Data\',Hemi{hemi},'_',Tone{tone},'.txt'));
-        data_time = data(:,1);
-        data = data(:,2);
+        data_time = data(:,1);%time
+        data = data(:,2);%AEF
+        
         % load simulation
-        model = load(strcat('HNN_Simulations\',Hemi{hemi},'_',...
+        simulation = load(strcat('HNN_Simulations\',Hemi{hemi},'_',...
                              Tone{tone},'\dpl.txt'));
-        model_time = model(:,1);
-        model = model(:,2);
+        simulation_time = simulation(:,1);%time
+        simulation_agg = simulation(:,2);%aggregate dipole
+        
         %plot
         plot(data_time, data,'Color', colours{1,tone}./255,'Linewidth',4);
-        plot(model_time, model, 'Color', colours{2,tone}./255, 'Linewidth', 2)
+        plot(simulation_time, simulation_agg,...
+             'Color', colours{2,tone}./255, 'Linewidth', 2)
     end
-
 end
 legend('contra data','contra model','ipsi data', 'ipsi model')        
                   
